6f5641160bb50184c53ade322cf5443af852351b
sikwan <yysskk99999@naver.com>
2024-02-01 16:45:09 +0900

    change

--- Ex/ViewController.swift
+++ Ex/ViewController.swift
@@ -23,51 +23,56 @@ class ViewController: UIViewController {
       
 
         
-            self.receiveLogs(company: "yangcompany1")
-            self.receiveLogs(company: "yangcompany1")
+            self.receiveLogs()
+            self.receiveLogs()
             sleep(1)
-            self.emitLog(company: "yangcompany1")
+            self.emitLog()
         
         
         
     }
         func createConnection() -> RMQConnection {
 
-//        http://username:password@yoursitename.com
+
             let uri = "amqp://neo:01579@112.219.138.170:5672"
             let delegate = RMQConnectionDelegateLogger()
             let connection = RMQConnection(/*uri:uri,*/delegate: delegate)
             return connection
         }
     
-    func emitLog(company : String) {
+    func emitLog() {
         
         let conn = createConnection()
         conn.start()
         let ch = conn.createChannel()
-        let x = ch.fanout("logs")
+        let x = ch.fanout("logs99yang123")
         let msg = "Hello World!"
-        let q = ch.queue(company)
+        let q = ch.queue("",options : .exclusive)
         ch.defaultExchange().publish(msg.data(using: .utf8)!,routingKey: q.name)
         
         
 //        x.publish(msg.data(using: String.Encoding.utf8)!)
         print("Sent \(msg)")
+        print(q.name)
        // conn.close()
     }
 
-    func receiveLogs(company : String) {
+    func receiveLogs() {
         let conn = createConnection()
         conn.start()
         let ch = conn.createChannel()
         
-        let x = ch.fanout("logs")
-        let q = ch.queue(company)
+        let x = ch.fanout("logs99yang123")
+        let q = ch.queue("",options: .exclusive)
+        
+      
         q.bind(x)
-        print("Waiting for logs.")
+        print("Waiting for logs.")//여기 까지왔자나
         
         q.subscribe({(_ message: RMQMessage) -> Void in
             print("Received \(String(data: message.body, encoding: String.Encoding.utf8)!)")
+            print("여기까진 오니?")
+           print(message)
         })
         
 
@@ -75,67 +80,4 @@ class ViewController: UIViewController {
     
    
 }
-//
-//import UIKit
-//import RMQClient
-//
-//class ViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.receiveLogs(exchangeName: "logs") // Receiver 설정 추가
-//        sleep(1)
-//        self.emitLog()
-//    }
-//
-//    func createConnection() -> RMQConnection {
-//        let uri = "amqp://neo:01579@112.219.138.170:5672"
-//        let delegate = RMQConnectionDelegateLogger()
-//        let connection = RMQConnection(/*uri: uri,*/ delegate: delegate)
-//        return connection
-//    }
-//
-//    func emitLog() {
-//        let conn = createConnection()
-//        conn.start()
-//        let ch = conn.createChannel()
-//        let x = ch.fanout("logs")
-//        let msg = "Hello World!"
-//        let q = ch.queue("neotest")
-//        ch.defaultExchange().publish(msg.data(using: .utf8)!, routingKey: q.name)
-//        print("Sent \(msg)")
-//    }
-//
-//    func receiveLogs(exchangeName: String) {
-//        let conn = createConnection()
-//        conn.start()
-//        let ch = conn.createChannel()
-//        let x = ch.fanout("logs")
-//        let q = ch.queue("neotest", options: .exclusive)
-//        q.bind(x)
-//
-//        print("Waiting for logs.")
-//
-//        q.subscribe({(_ message: RMQMessage) -> Void in
-//            if let messageString = String(data: message.body, encoding: .utf8) {
-//                print("Received \(messageString)")
-//
-//            }
-//        })
-//    }
-//}
-////func receiveLogs() {
-////        let conn = createConnection()
-////        conn.start()
-////        let ch = conn.createChannel()
-////
-////        let x = ch.fanout("logs")
-////        let q = ch.queue("neotest")
-////        q.bind(x)
-////        print("Waiting for logs.")
-////
-////        q.subscribe({(_ message: RMQMessage) -> Void in
-////            print("Received \(String(data: message.body, encoding: String.Encoding.utf8)!)")
-////        })
-////
-////
+//amqp://neo:01579@112.219.138.170:5672
